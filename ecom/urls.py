ab05332ae575d49e2faed77d9cd2f22e40549380
Dpaace <dipeshnepali767@gmail.com>
2022-02-11 02:54:39 +0545

    Dynamic Navbar

--- ecom/urls.py
+++ ecom/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from ecom import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('about', views.about, name='about'),
 ]
